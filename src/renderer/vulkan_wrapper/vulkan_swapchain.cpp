9faff660cee11f34e6ae7e3198471322b1a6395c
Tahar <tntmeijs@gmail.com>
2019-05-12 20:08:32 +0200

    WIP swapchain abstraction

--- src/renderer/vulkan_wrapper/vulkan_swapchain.cpp
+++ src/renderer/vulkan_wrapper/vulkan_swapchain.cpp
@@ -1,5 +1,6 @@
 // Application
 #include "miscellaneous/exceptions.hpp"
+#include "vulkan_device.hpp"
 #include "vulkan_instance.hpp"
 #include "vulkan_swapchain.hpp"
 
@@ -26,6 +27,15 @@ void VulkanSwapchain::CreateSurface(
 	}
 }
 
+void VulkanSwapchain::Create(const VulkanDevice& device) noexcept(false)
+{
+	// Query the swapchain support details
+	m_support_details = QuerySwapchainSupport(device);
+
+	// Create the swapchain itself
+	m_swapchain = CreateSwapchain(device);
+}
+
 void VulkanSwapchain::DestroySurface(const VulkanInstance& instance) const noexcept(true)
 {
 	vkDestroySurfaceKHR(instance.GetNative(), m_surface, nullptr);
@@ -34,4 +44,91 @@ void VulkanSwapchain::DestroySurface(const VulkanInstance& instance) const noexc
 const VkSurfaceKHR& VulkanSwapchain::GetSurfaceNative() const noexcept(true)
 {
 	return m_surface;
+}
+
+SwapchainSupportDetails VulkanSwapchain::QuerySwapchainSupport(
+	const VulkanDevice& device) const noexcept(false)
+{
+	std::uint32_t format_count = 0;
+	std::uint32_t present_mode_count = 0;
+	SwapchainSupportDetails support_details = {};
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(
+		device.GetPhysicalDeviceNative(),
+		m_surface,
+		&support_details.capabilities);
+
+	// Query the surface formats
+	vkGetPhysicalDeviceSurfaceFormatsKHR(
+		device.GetPhysicalDeviceNative(),
+		m_surface,
+		&format_count,
+		nullptr);
+
+	if (format_count == 0)
+	{
+		// No valid format available
+		throw exception::CriticalVulkanError("No valid surface format found.");
+	}
+
+	// Store the surface formats
+	support_details.formats.resize(format_count);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(
+		device.GetPhysicalDeviceNative(),
+		m_surface,
+		&format_count,
+		support_details.formats.data());
+
+	// Query the surface present modes
+	vkGetPhysicalDeviceSurfacePresentModesKHR(
+		device.GetPhysicalDeviceNative(),
+		m_surface,
+		&present_mode_count,
+		nullptr);
+
+	if (present_mode_count == 0)
+	{
+		// No valid present mode available
+		throw exception::CriticalVulkanError("No valid surface present mode found.");
+	}
+
+	// Store the surface present modes
+	support_details.present_modes.resize(present_mode_count);
+	vkGetPhysicalDeviceSurfacePresentModesKHR(
+		device.GetPhysicalDeviceNative(),
+		m_surface,
+		&present_mode_count,
+		support_details.present_modes.data());
+
+	if (support_details.formats.empty() ||
+		support_details.present_modes.empty())
+	{
+		throw exception::CriticalVulkanError("Swapchain support is not adequate.");
+	}
+
+	return support_details;
+}
+
+VkSwapchainKHR VulkanSwapchain::CreateSwapchain(
+	const VulkanDevice& device,
+	const SwapchainSupportDetails& support_details) const noexcept(false)
+{
+	VkSwapchainKHR swapchain;
+
+	// Find the best surface format to use
+	auto format = FindBestSurfaceFormat(support_details);
+
+
+
+
+
+
+	return swapchain;
+}
+
+VkSurfaceFormatKHR VulkanSwapchain::FindBestSurfaceFormat(
+	const SwapchainSupportDetails& support_details) const noexcept(true)
+{
+	CONTINUE HERE
+	return VkSurfaceFormatKHR();
 }
--- src/renderer/vulkan_wrapper/vulkan_swapchain.hpp
+++ src/renderer/vulkan_wrapper/vulkan_swapchain.hpp
@@ -4,18 +4,29 @@
 // Vulkan
 #include <vulkan/vulkan.h>
 
+// C++ standard
+#include <vector>
+
 namespace vkc
 {
 	class Window;
 
+	struct SwapchainSupportDetails
+	{
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> present_modes;
+	};
+
 	namespace vk_wrapper
 	{
+		class VulkanDevice;
 		class VulkanInstance;
 
 		class VulkanSwapchain
 		{
 		public:
-			VulkanSwapchain() noexcept(true) : m_surface(VK_NULL_HANDLE) {}
+			VulkanSwapchain() noexcept(true) : m_surface(VK_NULL_HANDLE), m_swapchain(VK_NULL_HANDLE) {}
 			~VulkanSwapchain() noexcept(true) {}
 
 			/** Use GLFW to create a surface */
@@ -23,13 +34,34 @@ namespace vkc
 				const VulkanInstance& instance,
 				const Window& window) noexcept(false);
 
+			/** Create the swapchain */
+			void Create(const VulkanDevice& device) noexcept(false);
+
 			/** Destroy the swapchain surface */
 			void DestroySurface(const VulkanInstance& instance) const noexcept(true);
 
+			/** Get a reference to the surface object */
 			const VkSurfaceKHR& GetSurfaceNative() const noexcept(true);
 
 		private:
+			/** Check whether a valid swapchain can be created */
+			SwapchainSupportDetails QuerySwapchainSupport(
+				const VulkanDevice& device) const noexcept(false);
+
+			/** Create the Vulkan swapchain object */
+			VkSwapchainKHR CreateSwapchain(
+				const VulkanDevice& device,
+				const SwapchainSupportDetails& support_details) const noexcept(false);
+
+			/** Find the best suited swapchain surface format */
+			VkSurfaceFormatKHR FindBestSurfaceFormat(
+				const SwapchainSupportDetails& support_details) const noexcept(true);
+
+		private:
 			VkSurfaceKHR m_surface;
+			VkSwapchainKHR m_swapchain;
+
+			SwapchainSupportDetails m_support_details;
 		};
 	}
 }
