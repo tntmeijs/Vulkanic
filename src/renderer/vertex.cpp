7f961fe7e528bb170cc800d1de074ada58c802cf
Tahar <tntmeijs@gmail.com>
2019-07-16 22:06:25 +0200

    Added empty files for the vertex wrapper class

---
+++ src/renderer/vertex.cpp
@@ -0,0 +1,10 @@
+// Application
+#include "vertex.hpp"
+
+using namespace vkc;
+
+Vertex::Vertex()
+{}
+
+Vertex::~Vertex()
+{}
---
+++ src/renderer/vertex.hpp
@@ -0,0 +1,14 @@
+#ifndef VERTEX_HPP
+#define VERTEX_HPP
+
+namespace vkc
+{
+    class Vertex
+    {
+	public:
+		Vertex() noexcept(true);
+		~Vertex() noexcept(true);
+    };
+}
+
+#endif // VERTEX_HPP
