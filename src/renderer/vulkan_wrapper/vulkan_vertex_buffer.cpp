ba74ca7360e4c931f3b4c111bd8573cf0c8108f0
Tahar <tntmeijs@gmail.com>
2019-07-17 13:23:11 +0200

    Added empty files for the vertex buffer abstraction

---
+++ src/renderer/vulkan_wrapper/vulkan_vertex_buffer.cpp
@@ -0,0 +1,10 @@
+// Application
+#include "vulkan_vertex_buffer.hpp"
+
+using namespace vkc::vk_wrapper;
+
+VulkanVertexBuffer::VulkanVertexBuffer() noexcept(true)
+{}
+
+VulkanVertexBuffer::~VulkanVertexBuffer() noexcept(true)
+{}
---
+++ src/renderer/vulkan_wrapper/vulkan_vertex_buffer.hpp
@@ -0,0 +1,17 @@
+#ifndef VULKAN_VERTEX_BUFFER_HPP
+#define VULKAN_VERTEX_BUFFER_HPP
+
+namespace vkc::vk_wrapper
+{
+	/** Wrapper class that abstracts vertex buffer creation */
+	class VulkanVertexBuffer
+	{
+	public:
+		VulkanVertexBuffer() noexcept(true);
+		~VulkanVertexBuffer() noexcept(true);
+
+	private:
+	};
+}
+
+#endif // VULKAN_VERTEX_BUFFER_HPP
